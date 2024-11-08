a82b196832157bf1f5842715ea1362f44bd56b9d
Amir Daghestani <ahdaghestani@live.com>
2024-11-08 12:15:03 +0100

    Add Backtracking Line Search.

--- sparse_federated_learning.py
+++ sparse_federated_learning.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import numpy as np
 import random
 from torchvision import datasets, transforms
+import cvxpy as cp
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -34,7 +35,7 @@ class Client:
         self.malicious = malicious
         self.attack_epoch = attack_epoch
     
-    def local_update(self, global_weights, epoch, return_avg_loss=True):
+    def local_update(self, global_weights, epoch, return_avg_loss=True, compute_gradient=True):
         """Client performs local update and sends gradients back to the server."""
         local_model = ThreeLayerFC().to(device)
         local_model.load_state_dict(global_weights)
@@ -52,18 +53,22 @@ class Client:
 
             output = local_model(data)
             loss = nn.CrossEntropyLoss()(output, target)
-            loss.backward()
+            if compute_gradient:
+                loss.backward()
             
             total_loss += loss.item()
             num_batches += 1
         
-        # Collect gradients
-        grads = [param.grad.clone() / num_batches for param in local_model.parameters()]
-
         avg_loss = None
+        grads = None
+
         if return_avg_loss:
             avg_loss = total_loss / num_batches
-        
+
+        if compute_gradient:
+            # Collect gradients
+            grads = [param.grad.clone() / num_batches for param in local_model.parameters()]
+
         return grads, avg_loss
 
 class Server:
@@ -107,7 +112,7 @@ class Server:
 
         return output
 
-    def federated_learning(self, alpha_vec, beta_vec, is_ftotal=True, lambda_val=(0, 0.05), is_norm_one=True,
+    def federated_learning(self, alpha_vec, beta_vec, is_ftotal=True, lambda_val=(0, 0.05, None), is_norm_one=True,
                            inner_iteration_range=3, alpha_decay_param=0.9, beta_decay_param=0.9):
         counter = -1
         if not isinstance(alpha_vec, list):
@@ -115,8 +120,10 @@ class Server:
         
         if not isinstance(beta_vec, list):
             beta_vec = [beta_vec]
-
-        lambda_range = np.linspace(lambda_val[0], lambda_val[1], self.total_epochs)
+        
+        lambda_range = list(np.linspace(lambda_val[0], lambda_val[1], lambda_val[-1] if lambda_val[-1] else self.total_epochs))
+        if len(lambda_range) != self.total_epochs:
+            lambda_range.extend([lambda_val[1]] * (self.total_epochs - len(lambda_range)))
 
         for i in range(len(alpha_vec)):
             counter += 1
@@ -142,9 +149,52 @@ class Server:
 
             G_next = G
             for epoch in range(self.total_epochs):
-                alpha = alpha_vec[i]
                 beta = beta_vec[i]
                 print(f"Epoch {epoch+1}/{self.total_epochs}")
+
+
+                # Backtracking Line Search for alpha
+                alpha = alpha_vec[i]
+                params_new = {key: val.clone() for key, val in self.global_model.state_dict().items()} # Not sure about this.
+                # Armijo condition
+                c = 1e-4
+                rho = 0.5
+                for _ in range(0):
+                    # Compute aggregated gradient
+                    agg_grad_vector = []
+                    with torch.no_grad():
+                        for param_idx, (name, param) in enumerate(self.global_model.named_parameters()):
+                            # Initialize the aggregated gradient
+                            agg_grad = torch.zeros_like(param.data)
+                            # Aggregate gradients weighted by w
+                            for client_idx in range(num_clients):
+                                grad = G[client_idx][param_idx]
+                                agg_grad += w[client_idx] * grad
+                            params_new[name] = param - alpha * agg_grad  # Update rule
+                            agg_grad_vector.append(agg_grad)
+
+                    # Evaluate new loss without updating local models or computing gradients
+                    new_client_losses = []
+                    for client in self.clients:
+                        _, avg_loss = client.local_update(params_new, epoch, return_avg_loss=True, compute_gradient=False)
+                        new_client_losses.append(avg_loss)
+                    L_new = np.mean(new_client_losses)
+                    L_old = np.mean(F_T)
+
+                    agg_grad_tensor = [tensor.view(-1) for tensor in agg_grad_vector]
+                    agg_grad_tensor = torch.concat(agg_grad_tensor)
+
+                    lhs = L_new
+                    rhs = L_old - c * alpha * np.linalg.norm(agg_grad_tensor) ** 2  # agg_grad_vector is the flattened agg_grad
+                    if lhs <= rhs:
+                        break
+                    else:
+                        alpha *= rho
+                else:
+                    print("Line search for alpha failed to find a suitable step size.")
+                    # You can decide whether to continue with current alpha or stop
+
+
 
                 for inner_theta_iteration in range(inner_iteration_range):
                     ######################################
@@ -177,7 +227,7 @@ class Server:
                         client_losses.append(avg_loss)
                         client_gradients.append(grads)
 
-                    alpha *= alpha_decay_param
+                    # alpha *= alpha_decay_param
                     # Construct G (gradient matrix) and F_T (loss vector)
                     G_next = client_gradients  # List of gradients from each client
                 F_T_next = client_losses   # List of losses from each client
@@ -185,7 +235,7 @@ class Server:
                 avg_loss = sum(F_T_next) / num_clients
                 print(f"Average Loss After Theta Update: {avg_loss}")
 
-                alpha = alpha_vec[i] # Reset
+                # alpha = alpha_vec[i] # Reset
 
                 G_flat = self._flatten_tensors(G)
                 for inner_w_iteration in range(inner_iteration_range):
@@ -195,16 +245,45 @@ class Server:
                     F_T_next_tensor = torch.tensor(F_T_next).to(device).to(torch.float32)
 
                     G_T_G_next = torch.matmul(G_flat.T, G_next_flat)
-                    G_T_G_next_w = torch.matmul(G_T_G_next, w_tensor)
-                    if is_ftotal:
-                        m_next = (w_tensor + beta * alpha * G_T_G_next_w - beta * F_T_next_tensor).tolist()
-                    else:
-                        m_next = (w_tensor + beta * alpha * G_T_G_next_w).tolist()
-                    self.list_m_next[counter].append(m_next)
+                    # G_T_G_next += 1e-4 * torch.eye(G_T_G_next.shape[0])
 
-                    w_next = self._norm_calculate(m_next, lambda_range[epoch], is_norm_one)
-                    w_next_normalize = self._normalize(w_next)
+                    G_T_G_next_w = torch.matmul(G_T_G_next, w_tensor)
+
+                    c = 1e-2
+                    rho = 0.5
+                    for _ in range(1):
+                        if is_ftotal:
+                            m_next = (w_tensor + beta * alpha * G_T_G_next_w - beta * F_T_next_tensor).tolist()
+                        else:
+                            m_next = (w_tensor + beta * alpha * G_T_G_next_w).tolist()
+
+                        # w_next_normalize = self._sparse_projection_onto_simplex(m_next, lambda_range[epoch])
+                        # self.list_w_next[counter].append(w_next_normalize)
+                        # Project onto simplex
+                        w_next_normalize = self._sparse_projection_onto_simplex(m_next, lambda_range[epoch])
+
+                        # Compute surrogate loss or criterion
+                        # Since we may not have an explicit loss function for w, you might define a custom criterion
+                        # For example, the norm of the change in w
+
+                        f_new = 0.5 * np.linalg.norm(np.array(w_next_normalize) - np.array(m_next)) ** 2
+                        f_current = 0.5 * np.linalg.norm(np.array(w) - np.array(m_next))**2
+
+                        grad_f_T = np.transpose(np.array(w) - np.array(m_next))
+                        armijo_condition = f_new <= f_current + c * beta * grad_f_T @ (np.array(w_next_normalize) - np.array(w))
+
+                        # rhs = np.linalg.norm(np.array(w) - np.array(m_next)) ** 2 + c * beta * np.matmul((np.array(w) - np.array(m_next)).transpose(), np.array(np.array(w_next_normalize) - np.array(w)))
+                        if armijo_condition:
+                            break
+                        else:
+                            beta *= rho
+                    else:
+                        print("Line search for beta failed to find a suitable step size.")
+                        # Decide whether to proceed or adjust beta differently
+
+                    self.list_m_next[counter].append(m_next)
                     self.list_w_next[counter].append(w_next_normalize)
+
 
                     # Calculate G_next, F_T_next
                     w = w_next_normalize
@@ -229,8 +308,8 @@ class Server:
                     G_next = client_gradients
                     F_T_next = client_losses
                     
-                    alpha *= alpha_decay_param
-                    beta *= beta_decay_param
+                    # alpha *= alpha_decay_param
+                    # beta *= beta_decay_param
 
                 avg_loss = sum(F_T_next) / num_clients
                 print(f"Average Loss After Weights Update: {avg_loss}")
@@ -248,6 +327,37 @@ class Server:
                 w_next[index_order[i]] -= lambda_value
         return w_next
 
+    def _sparse_projection_onto_simplex(self, m_next, lambda_value):
+        w = sorted(m_next, reverse=True)
+        index_order = np.flip(np.argsort(m_next))
+        index_order_1 = [f for f, value in enumerate(w) if value > lambda_value]
+        P_L_lambda = [w[i] for i in index_order_1]
+
+        if len(P_L_lambda) == 0:
+            return  [0] * len(w)
+
+        cumulative_sum = np.cumsum(P_L_lambda)
+        condition = P_L_lambda > (cumulative_sum - 1) / np.arange(1, len(P_L_lambda) + 1)
+        if np.any(condition):
+            rho = np.max(np.where(condition)[0] + 1)
+            etha = 1 / rho * (cumulative_sum[rho - 1] - 1)
+        else:
+            # raise Exception("Rho is empty!")
+            etha = cumulative_sum[-1] / len(P_L_lambda)
+
+        P_L_lambda_ehta = P_L_lambda - etha
+        P_plus = np.clip(P_L_lambda_ehta, 0, max(P_L_lambda_ehta)).tolist()
+        beta_S = [0] * len(w)
+
+        for i in range(len(index_order_1)):
+            beta_S[index_order_1[i]] = P_plus[i]
+
+        w_final = [0] * len(w)
+        for i in range(len(index_order)):
+            w_final[index_order[i]] = beta_S[i]
+
+        return w_final
+
     def _normalize(self, w_next):
         w_norm_one = sum(w_next)
         if w_norm_one != 0:
@@ -261,11 +371,11 @@ if __name__ == "__main__":
     # Initialize server and start federated learning
     num_clients = 10  # Total number of clients
     fraction_malicious = 0.3  # Fraction of malicious clients (e.g., 40%)
-    attack_epoch = 1  # Malicious clients start label-flipping after this epoch
-    total_epochs = 20  # Total number of epochs
+    attack_epoch = 0  # Malicious clients start label-flipping after this epoch
+    total_epochs = 30  # Total number of epochs
     n_max = 4  # Maximum number of non malicious clients
-    alpha_vec = [0.05]
-    beta_vec = [0.01]
+    alpha_vec = [0.25]  # Alpha vector
+    beta_vec = [0.025]  # Beta vector
     server = Server(num_clients=num_clients, fraction_malicious=fraction_malicious, attack_epoch=attack_epoch, total_epochs=total_epochs, n_max=n_max)
-    server.federated_learning(alpha_vec=alpha_vec, beta_vec=beta_vec, is_ftotal=True, lambda_val=(0, 0.01), is_norm_one=True,
-                              inner_iteration_range=3, alpha_decay_param=0.9, beta_decay_param=0.9)
+    server.federated_learning(alpha_vec=alpha_vec, beta_vec=beta_vec, is_ftotal=True, lambda_val=(0, 0.05, 20), is_norm_one=True,
+                              inner_iteration_range=1, alpha_decay_param=0.9, beta_decay_param=0.9)
