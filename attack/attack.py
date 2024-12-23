Fix our method gradient estimation.

attack/attack.py
@@ -1,5 +1,7 @@
 """ Attack Schemes """
 import torch
+from scipy.stats import norm
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -36,7 +38,43 @@ class Attack:
         return [split.view(shape).to(device) for split, shape in zip(torch.split(grads_concat, split_size), original_shapes)]
 
     def lie_attack(*args, **kwargs):
-        return [torch.normal(mean=torch.mean(grad) + kwargs['lie_attack_factor'] * torch.std(grad), std=torch.std(grad), size=grad.shape).to(device) for grad in kwargs['grads']]
+        clients = kwargs['clients']
+        clients_grads = kwargs['grads']
+        num_clients = len(clients)
+
+        malicious_gradients = [
+            clients_grads[i]
+            for i, client in enumerate(clients) if client.malicious
+        ]
+
+        s = num_clients // 2 + 1 - len(malicious_gradients)
+        phi_value = (num_clients - s) / num_clients
+        z = norm.ppf(phi_value)
+
+        # Initialize dictionary to store crafted malicious gradient
+        attacked_grad = {}
+
+        if isinstance(clients_grads[0], dict):
+            for_list = clients_grad[0].keys()
+        else:
+            for_list = range(len(clients_grads[0]))
+
+        # Stack tensors for each key in the gradient dictionaries
+        for key in for_list:
+            # Extract tensors for the current key from all malicious gradients
+            stacked_tensors = torch.stack([grad[key].float() for grad in malicious_gradients])
+            mean_tensor = torch.mean(stacked_tensors, dim=0)
+            std_tensor = torch.std(stacked_tensors, dim=0)
+
+            # Craft the malicious gradient for the current key
+            attacked_grad[key] = mean_tensor - z * std_tensor
+
+        # Replace gradients for malicious clients
+        for i, client in enumerate(clients):
+            if client.malicious:
+                clients_grads[i] = attacked_grad
+
+        return clients_grads
 
     def __call__(attack_func, *args, **kwargs):
         return attack_func(*args, **kwargs)

client/client.py
@@ -12,7 +12,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class Client:
     ATTACK_ON_DATA = ['flip_labels']
     ATTACK_ON_PARAMETRS = ['random_parameters']
-    ATTACK_ON_GRADIENT = ['boost_gradient', 'gaussian_attack', 'gaussian_additive_attack', 'lie_attack']
+    ATTACK_ON_GRADIENT = ['boost_gradient', 'gaussian_attack', 'gaussian_additive_attack']
 
     def __init__(self, client_id, model, data_loader, local_epoch=1, malicious=False, attack_args=None):
         self.client_id = client_id
@@ -58,9 +58,11 @@ class Client:
             global_weights_random = self.attack_func(global_weights=global_weights, **self.attack_args)
             local_model.load_state_dict(global_weights_random)
 
+        total_grads = None
         for epoch in range(self.local_epoch):
             total_loss = 0
             num_batches = 0
+            grad_trajectory = []
 
             for data, target in self.data_loader:
                 data, target = data.to(device), target.to(device)
@@ -89,7 +91,17 @@ class Client:
                         for param, grad in zip(local_model.parameters(), grads):
                             param.grad = grad
 
-                    optimizer.step()
+                    # Initialize total_grads if it's the first batch
+                    if total_grads is None:
+                        total_grads = [torch.zeros_like(grad) for grad in grads]
+
+                    for i, grad in enumerate(grads):
+                        total_grads[i] += grad
+
+                    # optimizer.step()
+                    for i, param in enumerate(local_model.parameters()):
+                        param.data =  param.data - lr * grads[i]
+
 
                 total_loss += loss.item()
                 num_batches += 1
@@ -97,6 +109,8 @@ class Client:
             if epoch == self.local_epoch - 1:
                 avg_loss = total_loss / num_batches if return_avg_loss else None
 
+        grads = total_grads
+        
         if return_params:
             params = {key: local_model.state_dict()[key] - global_weights[key] for key in global_weights.keys()}
         else:

server/server.py
@@ -10,11 +10,14 @@ import wandb
 
 from model.model import SimpleCNNWithBatchNorm, PyTorchLeNet5, ThreeLayerFC
 from client.client import Client
+from attack.attack import Attack
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Server:
+    ATTACK_ON_BENIGN_UPDATES = ['lie_attack']
+
     def __init__(self, dataset_name, num_clients, fraction_malicious, attack_args=None, total_epochs=5, q_factor=0.1, model=SimpleCNNWithBatchNorm(), evaluate_each_epoch=2, local_epochs=1):
         self.global_model = model.to(device)
         self.global_model_fedavg = copy.deepcopy(model).to(device)
@@ -26,6 +29,15 @@ class Server:
         self.evaluate_each_epoch = evaluate_each_epoch
         self.list_m_next = []
         self.list_w_next = []
+
+        self.attack_args = attack_args
+        if attack_args is not None:
+            self.attack_type = attack_args['attack_type']
+            self.attack_epoch = attack_args['attack_epoch']
+
+            # Attack on Gradient
+            if self.attack_type == 'lie_attack':
+                self.attack_func = Attack.lie_attack
 
     def _initialize_clients(self, dataset_name, num_clients, model, fraction_malicious, attack_args, q_factor):
         self.num_clients = num_clients
@@ -114,7 +126,7 @@ class Server:
             for j, c in enumerate(client_per_group_idx):
                 client2data_idx[str(c)] = list_clients[j]
         
-        client_loaders = [torch.utils.data.DataLoader(torch.utils.data.Subset(train_dataset, client2data_idx[str(i)]), batch_size=64, shuffle=True) for i in range(self.num_clients)]
+        client_loaders = [torch.utils.data.DataLoader(torch.utils.data.Subset(train_dataset, client2data_idx[str(i)]), batch_size=128, shuffle=True) for i in range(self.num_clients)]
 
         return client_loaders
 
@@ -251,6 +263,13 @@ class Server:
             grads, avg_loss = client.local_update(global_weights, epoch, return_avg_loss, compute_gradient, return_params=return_params, lr=lr)
             client_gradients.append(grads)
             client_losses.append(avg_loss)
+        
+        # Attack on Benign Updates
+        is_under_attack = epoch >= self.attack_epoch
+
+        if self.attack_type in self.ATTACK_ON_BENIGN_UPDATES and is_under_attack:
+            client_gradients = self.attack_func(grads=client_gradients, clients=self.clients, **self.attack_args)
+
         return client_gradients, client_losses
 
     def _line_search_alpha(self, alpha, G, F_T, w, num_clients, c, rho, epoch, max_iteration=3):